6fecf51b133f5a50f9d4a8bc39973254977e2012
enxa <cezary.walczak.pl@gmail.com>
2024-01-02 13:32:04 +0100

    Add integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), svelte()],
+  output: "server",
+  adapter: vercel()
+});
